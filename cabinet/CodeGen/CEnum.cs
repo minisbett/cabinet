270d1dbf999c7afe0fcdd02c46e555322ee719d9
minisbett <39670899+minisbett@users.noreply.github.com>
2026-01-20 17:49:26 +0100

    enforce C enum naming convention

--- cabinet/CodeGen/CEnum.cs
+++ cabinet/CodeGen/CEnum.cs
@@ -1,11 +1,12 @@
 ﻿using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace cabinet.CodeGen;
 
 /// <summary>
 /// Represents all information required to generate the source for a C-enum.
 /// </summary>
-internal class CEnum(string name, (string Name, int Value)[] values)
+internal class CEnum(string name, (string Name, object Value)[] values)
 {
   /// <summary>
   /// Returns the source code for this enum.
@@ -14,7 +15,7 @@ internal class CEnum(string name, (string Name, int Value)[] values)
   => $$"""
      typedef enum {{name}}
      {
-         {{string.Join(",\n    ", values.Select(x => $"{x.Name} = {x.Value}"))}}
+         {{string.Join(",\n    ", values.Select(x => $"{Regex.Replace(x.Name, @"([a-z0-9])([A-Z])|[\s\-]+", "$1_$2").ToUpper()} = {x.Value}"))}}
      } {{name}};
      """;
 }
